8a1fc72f5837796cfbb8e75d690b1379283401f5
Timothée Raboeuf <timothee.raboeuf@insa-rennes.fr>
2017-11-07 14:46:30 +0100

    Loading token from a config file

--- bot.js
+++ bot.js
@@ -1,32 +1,45 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client()
+const fs = require('fs');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
 bot.on('ready', function () {
-  console.log("Je suis connecté !")
+  console.log("Je suis connecté !");
 })
 
 bot.on('message', message => {
   if (message.content === 'ping') {
-    message.reply('pong !')
+    message.reply('pong !');
   }
   
   //Diplodocus
-  var arr = /[dD]i(\w+)\W?/g.exec(message.content)
+  var arr = /[dD]i(\w+)\W?/g.exec(message.content);
   if(arr != null) {
     for(i in arr) {
-      console.log(i + " => " + arr[i])
+      console.log(i + " => " + arr[i]);
     }
-    message.channel.send(arr[1] + " !")
+    message.channel.send(arr[1] + " !");
   }
   
   //Criplodocus
-  arr = /[Cc]ri(\w+)\W?/g.exec(message.content)
+  arr = /[Cc]ri(\w+)\W?/g.exec(message.content);
   if(arr != null) {
     for(i in arr) {
-      console.log(i + " => " + arr[i])
+      console.log(i + " => " + arr[i]);
     }
-    message.channel.send(arr[1].toUpperCase() + " !")
+    message.channel.send(arr[1].toUpperCase() + " !");
   }
 })
 
-bot.login('')
+fs.readFile('./config.json', 'utf-8', (err, data) => {
+  if(err) {
+    console.error("Error : could not open file config.json");
+    return;
+  }
+  data = JSON.parse(data);
+  if(!data.hasOwnProperty('token') || data.token == '' || data.token == undefined) {
+    console.error("Error : config.json does not contain a valid token. ");
+    return;
+  }
+  bot.login(data.token)
+});
+
