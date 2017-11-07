31fae7ae082105c817b0a188e288d8f732bc8950
Timothée Raboeuf <timothee.raboeuf@insa-rennes.fr>
2017-11-07 14:25:49 +0100

    Added diplodocus and criplodocus

--- bot.js
+++ bot.js
@@ -9,6 +9,24 @@ bot.on('message', message => {
   if (message.content === 'ping') {
     message.reply('pong !')
   }
+  
+  //Diplodocus
+  var arr = /[dD]i(\w+)\W?/g.exec(message.content)
+  if(arr != null) {
+    for(i in arr) {
+      console.log(i + " => " + arr[i])
+    }
+    message.channel.send(arr[1] + " !")
+  }
+  
+  //Criplodocus
+  arr = /[Cc]ri(\w+)\W?/g.exec(message.content)
+  if(arr != null) {
+    for(i in arr) {
+      console.log(i + " => " + arr[i])
+    }
+    message.channel.send(arr[1].toUpperCase() + " !")
+  }
 })
 
 bot.login('')
